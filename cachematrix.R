da90f4fa6d53a34eb92f1c40ed02d6fb4de882c3
satorthemiller <jhiggins73@gmail.com>
2014-09-19 19:41:28 -0400

    Committing this

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+##makeCacheMatrix pulls a cached matrix result back and forth. 
+##cacheSolve uses the cached result if it exists, otherwise calcs the inverse with Solve. 
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+ 
+  getcachedx <- function()  x    # this function returns the value of the original vector
+  setcachedxinv <- function(invertedresult){ # called by f below, pushes the inverse up to minv 
+      minv<<- invertedresult
+      }
+  getcachedxinv <- function() minv # pulls the inverse down 
 
+  list(getcachedx = getcachedx,  setcachedxinv = setcachedxinv, getcachedxinv=getcachedxinv)
 }
 
 
-## Write a short comment describing this function
+# This function uses Solve to get the matrix inversion, if it needs to.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ##  if a stored (previous) value of x exists, uses its stored inverse, , otherwise
+        ##  Returns a matrix that is the inverse of 'x'
+  
+  m<- x$getcachedx() #access the object x and gets the stored matrix
+  if(!is.null(m)) {
+        
+      message("getting cached inverse")
+      return(x$getcachedxinv())
+    } 
+        
+  data<- x$getcachedx()
+  minv <- solve(data)
+  x$setcachedxinv(minv)
+    
+  minv
 }
